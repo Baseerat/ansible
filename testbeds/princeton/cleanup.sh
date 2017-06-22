342bba8fc5e3be56d6409f53a3c1ceb47dd264b3
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-06-22 08:48:46 -0500

    added main setup.sh and cleanup.sh scripts

---
+++ testbeds/princeton/cleanup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#ansible-playbook kubernetes.yml --tags destroy_cluster
+#ansible-playbook kubernetes.yml --tags destroy_overlay
+#ansible-playbook common.yml --tags remove
+ansible-playbook proxmox.yml --tags destroy_vms
