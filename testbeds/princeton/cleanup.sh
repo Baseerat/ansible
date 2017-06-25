d9645ff26f9966afff4b14ed9b2d7492c67cd023
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-06-25 02:10:33 -0500

    commits with a note on resolving STP issue in GRE tunnels

--- testbeds/princeton/cleanup.sh
+++ testbeds/princeton/cleanup.sh
@@ -4,3 +4,4 @@
 #ansible-playbook kubernetes.yml --tags destroy_overlay
 #ansible-playbook common.yml --tags remove
 ansible-playbook proxmox.yml --tags destroy_vms
+ansible-playbook proxmox.yml --tags destroy_overlay
--- testbeds/princeton/setup.sh
+++ testbeds/princeton/setup.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+ansible-playbook proxmox.yml --tags create_overlay
 ansible-playbook proxmox.yml --tags create_vms
 ansible-playbook common.yml --tags install
 ansible-playbook kubernetes.yml --tags create_overlay
