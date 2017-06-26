6ccf6bc78912f16d6fe6d043475954bc1cd4419a
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-06-26 05:34:53 -0500

    added VMware testbed scripts

---
+++ testbeds/vmware/cleanup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#ansible-playbook kubernetes.yml --tags destroy_cluster
+#ansible-playbook kubernetes.yml --tags destroy_overlay
+#ansible-playbook common.yml --tags remove
+ansible-playbook proxmox.yml --tags destroy_vms
+ansible-playbook proxmox.yml --tags destroy_overlay
+ansible-playbook proxmox.yml --tags remove_vm_template
---
+++ testbeds/vmware/setup.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+#ansible-playbook proxmox.yml --tags add_vm_template
+ansible-playbook proxmox.yml --tags create_overlay
+ansible-playbook proxmox.yml --tags create_vms
+ansible-playbook common.yml --tags install
+ansible-playbook kubernetes.yml --tags create_overlay
+ansible-playbook kubernetes.yml --tags create_cluster
