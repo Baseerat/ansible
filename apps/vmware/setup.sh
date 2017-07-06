c717bcd74bd3a96e321f2bb276cd77d805051191
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-07-05 19:23:48 -0500

    commits post addition of collect lb workload

--- apps/vmware/setup.sh
+++ apps/vmware/setup.sh
@@ -5,5 +5,6 @@
 cp -f ../../testbeds/vmware/ansible.cfg .
 cp -f ../../testbeds/vmware/inventory.ini .
 cp -f ../../testbeds/vmware/ssh_config .
+cp -rf ../../testbeds/vmware/keys ./keys
 cp -f ../../testbeds/vmware/vars/kubernetes.yml ./vars
 cp -f ../../testbeds/vmware/roles/kubernetes/defaults/main.yml ./vars/kubernetes.defaults.yml
