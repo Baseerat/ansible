127bafc8d7583107971798d96fdbecc38deb9671
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-07-09 07:33:00 -0500

    scripts for making br0 the gateway bridge on the promox machines

--- testbeds/vmware/cleanup.sh
+++ testbeds/vmware/cleanup.sh
@@ -3,6 +3,7 @@
 #ansible-playbook kubernetes.yml --tags destroy_cluster
 #ansible-playbook kubernetes.yml --tags destroy_overlay
 #ansible-playbook common.yml --tags remove
+ansible-playbook proxmox.yml --tags cleanup_br0_as_primary_bridge
 ansible-playbook proxmox.yml --tags destroy_vms
 ansible-playbook proxmox.yml --tags destroy_overlay
 #ansible-playbook proxmox.yml --tags remove_vm_template
--- testbeds/vmware/setup.sh
+++ testbeds/vmware/setup.sh
@@ -3,6 +3,7 @@
 #ansible-playbook proxmox.yml --tags add_vm_template
 ansible-playbook proxmox.yml --tags create_overlay
 ansible-playbook proxmox.yml --tags create_vms
+ansible-playbook proxmox.yml --tags setup_br0_as_primary_bridge
 ansible-playbook common.yml --tags install
 ansible-playbook kubernetes.yml --tags create_overlay
 ansible-playbook kubernetes.yml --tags create_cluster
