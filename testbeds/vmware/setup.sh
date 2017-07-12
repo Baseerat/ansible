e12f9fbe2472c4868c65ab35c02b282ccaa5e2fc
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-07-12 06:12:27 -0500

    refactoring

--- testbeds/vmware/setup.sh
+++ testbeds/vmware/setup.sh
@@ -3,9 +3,9 @@
 #ansible-playbook proxmox.yml --tags add_vm_template
 ansible-playbook proxmox.yml --tags create_overlay
 ansible-playbook proxmox.yml --tags create_vms
+ansible-playbook proxmox.yml --tags setup_br0_as_primary_bridge
+sleep 30
 ansible-playbook common.yml --tags install
 ansible-playbook kubernetes.yml --tags create_overlay
 ansible-playbook kubernetes.yml --tags create_cluster
-ansible-playbook proxmox.yml --tags setup_br0_as_primary_bridge
-sleep 10
 #ansible-playbook kubernetes.yml --tags create_apps
