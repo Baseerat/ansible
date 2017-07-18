fbfdb3394476bbf6bc52e0e38b609aefebd6e41a
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-07-18 02:49:07 -0700

    minor

--- testbeds/vmware/setup.sh
+++ testbeds/vmware/setup.sh
@@ -2,11 +2,13 @@
 
 #ansible-playbook proxmox_tor.yml --tags install_pisces
 #ansible-playbook proxmox_tor.yml --tags setup_tor_network
+
 #ansible-playbook proxmox.yml --tags add_template
 ansible-playbook proxmox.yml --tags create_overlay
 ansible-playbook proxmox.yml --tags create_vms
 ansible-playbook proxmox.yml --tags setup_br0_as_primary_bridge
 sleep 30
+
 #ansible-playbook kubernetes.yml --tags install_common
 #ansible-playbook kubernetes.yml --tags create_overlay
 #ansible-playbook kubernetes.yml --tags create_cluster
