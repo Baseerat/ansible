9e0c8db9c0c885795695287f6947b18d4e015824
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-07-19 00:51:33 -0700

    minor

--- apps/vmware/cleanup.sh
+++ apps/vmware/cleanup.sh
@@ -7,7 +7,6 @@ rm -f ansible.cfg
 rm -f inventory.ini
 rm -f ssh_config
 rm -rf ./keys
-rm -f ./vars/kubernetes.yml
-rm -f ./vars/kubernetes.defaults.yml
+rm -rf ./vars/kubernetes
 
 #../../testbeds/vmware/cleanup.sh
