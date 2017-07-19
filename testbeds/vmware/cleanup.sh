5d57a2e665b5287eaf5a9c6d776333382180eb85
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-07-19 00:51:59 -0700

    minor

--- testbeds/vmware/cleanup.sh
+++ testbeds/vmware/cleanup.sh
@@ -4,6 +4,8 @@
 #ansible-playbook kubernetes.yml --tags destroy_overlay
 #ansible-playbook kubernetes.yml --tags remove_common
 
+#ansible-playbook baseerat.yml --tags cleanup
+
 ansible-playbook proxmox.yml --tags cleanup_br0_as_primary_bridge
 ansible-playbook proxmox.yml --tags destroy_vms
 ansible-playbook proxmox.yml --tags destroy_overlay
--- testbeds/vmware/setup.sh
+++ testbeds/vmware/setup.sh
@@ -9,6 +9,8 @@ ansible-playbook proxmox.yml --tags create_vms
 ansible-playbook proxmox.yml --tags setup_br0_as_primary_bridge
 sleep 30
 
+#ansible-playbook baseerat.yml --tags setup
+
 #ansible-playbook kubernetes.yml --tags install_common
 #ansible-playbook kubernetes.yml --tags create_overlay
 #ansible-playbook kubernetes.yml --tags create_cluster
