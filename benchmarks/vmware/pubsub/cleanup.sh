3f766c07a1849fcb2ee130fe80d6ba569f9eec45
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-07-30 02:24:57 -0700

    refactoring

--- benchmarks/vmware/pubsub/cleanup.sh
+++ benchmarks/vmware/pubsub/cleanup.sh
@@ -3,7 +3,7 @@
 #ansible-playbook mqtt_vms.yml --tags remove_common
 #ansible-playbook mqtt_vms.yml --tags cleanup_mqtt
 #ansible-playbook zeromq_vms.yml --tags remove_common
-#ansible-playbook zeromq_vms.yml --tags destroy_zeromq
+#ansible-playbook zeromq_vms.yml --tags destroy_fanout
 
 rm -f ansible.cfg
 rm -f inventory.ini
--- benchmarks/vmware/pubsub/setup.sh
+++ benchmarks/vmware/pubsub/setup.sh
@@ -10,5 +10,5 @@ cp -f ../../../testbeds/vmware/roles/proxmox/defaults/main.yml ./vars/proxmox/de
 #ansible-playbook mqtt_vms.yml --tags install_common
 #ansible-playbook mqtt_vms.yml --tags setup_mqtt
 #ansible-playbook zeromq_vms.yml --tags install_common
-#ansible-playbook zeromq_vms.yml --tags launch_zeromq
-#ansible-playbook zeromq_vms.yml --tags collect_stats
+#ansible-playbook zeromq_vms.yml --tags launch_fanout
+#ansible-playbook zeromq_vms.yml --tags collect_fanout_stats
