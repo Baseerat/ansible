5f427eb6d550dfd64c30cc0867c23aaf71e9a62c
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-07-30 00:34:38 -0700

    minor

--- benchmarks/vmware/pubsub/setup.sh
+++ benchmarks/vmware/pubsub/setup.sh
@@ -11,3 +11,4 @@ cp -f ../../../testbeds/vmware/roles/proxmox/defaults/main.yml ./vars/proxmox/de
 #ansible-playbook mqtt_vms.yml --tags setup_mqtt
 #ansible-playbook zeromq_vms.yml --tags install_common
 #ansible-playbook zeromq_vms.yml --tags launch_zeromq
+#ansible-playbook zeromq_vms.yml --tags collect_stats
