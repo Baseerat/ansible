ebeabb20195808de7c087055828d76574c99711a
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-08-06 04:54:50 -0700

    minor

--- testbeds/vmware/setup.sh
+++ testbeds/vmware/setup.sh
@@ -14,9 +14,9 @@ ansible-playbook proxmox.yml --tags create_vms
 ansible-playbook proxmox.yml --tags setup_br0_as_primary_bridge
 sleep 30
 
-ansible-playbook baseerat.yml --tags setup
+#ansible-playbook baseerat.yml --tags setup
 
-#ansible-playbook kubernetes.yml --tags install_common
-#ansible-playbook kubernetes.yml --tags create_overlay
-#ansible-playbook kubernetes.yml --tags create_cluster
-#ansible-playbook kubernetes.yml --tags create_apps
+ansible-playbook kubernetes.yml --tags install_common
+ansible-playbook kubernetes.yml --tags create_overlay
+ansible-playbook kubernetes.yml --tags create_cluster
+ansible-playbook kubernetes.yml --tags create_apps
