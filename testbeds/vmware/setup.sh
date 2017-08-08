bf40fcbbd63ba74c9f86b88e9cf011a3fc85f807
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-08-07 19:22:16 -0700

    minor

--- testbeds/vmware/setup.sh
+++ testbeds/vmware/setup.sh
@@ -1,20 +1,21 @@
 #!/bin/sh
 
-#ansible-playbook proxmox_dc.yml --tags install_pisces
-##ansible-playbook proxmox_dc.yml --tags install_ovs_dpdk
-#ansible-playbook proxmox_dc.yml --tags setup_dc_network
-#
-#ansible-playbook proxmox_tor.yml --tags install_pisces
-##ansible-playbook proxmox_tor.yml --tags install_ovs_dpdk
-#ansible-playbook proxmox_tor.yml --tags setup_tor_network
+ansible-playbook proxmox_dc.yml --tags install_pisces
+#ansible-playbook proxmox_dc.yml --tags install_ovs_dpdk
+ansible-playbook proxmox_dc.yml --tags setup_dc_network
+
+ansible-playbook proxmox_tor.yml --tags install_pisces
+#ansible-playbook proxmox_tor.yml --tags install_ovs_dpdk
+ansible-playbook proxmox_tor.yml --tags setup_tor_network
 
 #ansible-playbook proxmox.yml --tags add_template
 ansible-playbook proxmox.yml --tags setup_overlay
 ansible-playbook proxmox.yml --tags create_vms
+ansible-playbook proxmox.yml --tags add_br0_flow_rules
 #ansible-playbook proxmox.yml --tags setup_br0_as_primary_bridge
 sleep 30
 
-#ansible-playbook baseerat.yml --tags setup
+ansible-playbook baseerat.yml --tags setup
 
 #ansible-playbook kubernetes.yml --tags install_common
 #ansible-playbook kubernetes.yml --tags create_overlay
