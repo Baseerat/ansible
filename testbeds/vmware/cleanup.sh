9982a55ef88ac9117dfb43f7889e5371808efef6
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-08-24 06:37:03 -0700

    minor

--- testbeds/vmware/cleanup.sh
+++ testbeds/vmware/cleanup.sh
@@ -6,6 +6,7 @@
 #ansible-playbook kubernetes.yml --tags remove_common
 
 #ansible-playbook baseerat.yml --tags cleanup
+#ansible-playbook baseerat_dc_tor.yml --tags cleanup
 
 #ansible-playbook proxmox.yml --tags cleanup_br0_as_primary_bridge - deprecated!
 ansible-playbook proxmox.yml --tags remove_br0_flow_rules
