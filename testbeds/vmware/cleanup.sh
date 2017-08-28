04064b97393e3255e78a62272a5130669d538efd
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-08-28 01:35:13 -0500

    starting with LB results

--- testbeds/vmware/cleanup.sh
+++ testbeds/vmware/cleanup.sh
@@ -5,8 +5,8 @@
 #ansible-playbook kubernetes.yml --tags destroy_overlay
 #ansible-playbook kubernetes.yml --tags remove_common
 
-ansible-playbook baseerat.yml --tags cleanup
-ansible-playbook baseerat_dc_tor.yml --tags cleanup
+#ansible-playbook baseerat.yml --tags cleanup
+#ansible-playbook baseerat_dc_tor.yml --tags cleanup
 
 #ansible-playbook proxmox.yml --tags cleanup_br0_as_primary_bridge - deprecated!
 ansible-playbook proxmox.yml --tags remove_br0_flow_rules
--- testbeds/vmware/setup.sh
+++ testbeds/vmware/setup.sh
@@ -19,8 +19,8 @@ ansible-playbook proxmox.yml --tags add_br0_flow_rules
 #ansible-playbook proxmox.yml --tags setup_br0_as_primary_bridge - deprecated!
 #sleep 30
 
-ansible-playbook baseerat_dc_tor.yml --tags setup
-ansible-playbook baseerat.yml --tags setup
+#ansible-playbook baseerat_dc_tor.yml --tags setup
+#ansible-playbook baseerat.yml --tags setup
 
 #ansible-playbook kubernetes.yml --tags install_common
 #ansible-playbook kubernetes.yml --tags create_overlay
