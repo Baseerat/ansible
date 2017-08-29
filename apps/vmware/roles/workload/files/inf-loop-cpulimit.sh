754a41c5ffd4a9aa18714b5d3b018b917f1ccaf6
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-08-29 10:14:51 -0500

    minor (after adding command for docker update --cpu-sets)

--- apps/vmware/roles/workload/files/inf-loop-cpulimit.sh
+++ apps/vmware/roles/workload/files/inf-loop-cpulimit.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 for j in `seq 1 $3`; do
-    for i in `seq 1 $1`; do taskset -c $(($j-1)) cpulimit -l $2 inf-loop.sh & done
+    for i in `seq 1 $1`; do taskset -c $(($(($j-1))*$4)) cpulimit -l $2 inf-loop.sh & done
 done
