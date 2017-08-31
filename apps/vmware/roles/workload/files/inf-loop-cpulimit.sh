5aad91dc76e168228205b1debc8d3a8a07cff602
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-08-31 08:21:38 -0500

    minor

--- apps/vmware/roles/workload/files/inf-loop-cpulimit.sh
+++ apps/vmware/roles/workload/files/inf-loop-cpulimit.sh
@@ -2,4 +2,5 @@
 
 for j in `seq 1 $3`; do
     for i in `seq 1 $1`; do taskset -c $(($(($j-1))*$4)) cpulimit -l $2 inf-loop.sh & done
+#    for i in `seq 1 $1`; do nice --9 taskset -c $(($(($j-1))*$4)) inf-loop.sh & done
 done
