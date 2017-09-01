216c51deabb75fc6c4a5ad2ca0b6894f0812eaf6
Muhammad Shahbaz <mshahbaz@users.noreply.github.com>
2017-09-01 05:55:04 -0500

    minor

--- apps/vmware/roles/workload/files/stress-ng-host-rand.sh
+++ apps/vmware/roles/workload/files/stress-ng-host-rand.sh
@@ -9,8 +9,9 @@ while : ; do
   if [ $FLIP -eq 1 ]
   then
     taskset -c $2 stress-ng --cpu $3 &
+    PID=$!
     sleep $4
-    pkill -u root -f stress-ng
+    pkill -TERM -P ${PID}
   else
     sleep $4
   fi
